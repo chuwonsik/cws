d7576f3f9a0ebeee255dc0e337bf973e1e86a0e3
chu <eggzzang1@gmail.com>
2013-05-09 20:54:42 +0900

    test

---
+++ Issac/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  IssacAppController.h
+//  Issac
+//
+//  Created by won-sik Chu on 13. 5. 2..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Issac/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  IssacAppController.h
+//  Issac
+//
+//  Created by won-sik Chu on 13. 5. 2..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
